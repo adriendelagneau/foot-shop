0d50dec671bbd8b61da5c98656a63accb464bc9d
adriendelagneau <delagneauadrien@yahoo.fr>
2022-10-30 10:09:01 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { ThemeProvider } from 'next-themes'
+import { Fragment } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <Fragment>
+    <ThemeProvider enableSystem={true} attribute="class">
+    <Component {...pageProps} />
+    </ThemeProvider>
+    </Fragment>
+  )
 }
 
 export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,69 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      animation : {
+        'rotateStar': 'rotateStar 1.2s linear ',
+        'tourniquet': 'tourniquet  10s linear infinite',
+      },
+      keyframes: {
+        rotateStar: {
+          '0%': { transform: 'rotateY(90deg)'},
+          '100%': { transform: 'rotateY(0deg)' }
+        },
+        rotateCube: {
+          '0%': { transform: 'rotateX(45deg) rotateY(-45deg'},
+          '25%': { transform: 'rotateX(-45deg) rotateY(-45deg'},
+          '50%': { transform: 'rotateX(45deg) rotateY(45deg'},
+          '75%': { transform: 'rotateX(-45deg) rotateY(45deg'},
+          '100%': { transform: 'rotateX(45deg) rotateY(-45deg'},
+        },
+        tourniquet: {
+          '0%': { transform: 'rotateY(0deg)'},
+          '100%': { transform: 'rotateY(360deg)' }
+        },
+        loaderPart1: {
+          '37.5%': { transform: 'translateX(0px'},
+          '50%': { transform: 'translateX(100px'},
+          '100%': { transform: 'translateX(100px'},
+        },
+        loaderPart2: {
+          '25%': { transform: 'translateX(0px'},
+          '37.5%': { transform: 'translateX(100px'},
+          '100%': { transform: 'translateX(100px'},
+        },
+        loaderPart3: {
+          '12.5%': { transform: 'translateY(0px'},
+          '25%': { transform: 'translateY(100px'},
+          '100%': { transform: 'translateY(100px'},
+        },
+        loaderPart4: {
+          '37.5%': { transform: 'translateY(0px'},
+          '50%': { transform: 'translateY(-100px'},
+          '100%': { transform: 'translateY(-100px'},
+        },
+        loaderPart5: {
+          '12.5%': { transform: 'translate(*100px, 0px'},
+          '87.5%': { transform: 'translate(-100px, 0px'},
+          '100%': { transform: 'translate(-100px, 100px'},
+        },
+        loaderPart6: {
+          '62.5%': { transform: 'translateY(0px'},
+          '75%': { transform: 'translateY(-100px'},
+          '100%': { transform: 'translateY(-100px'},
+        },
+        loaderPart7: {
+          '75%': { transform: 'translateX(0px'},
+          '87.5%': { transform: 'translateX(-100px'},
+          '100%': { transform: 'translateX(-100px'},
+        }
+      }
+    }
+  },
+  plugins: [],
+}
